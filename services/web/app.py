4c7b80ba6818a3521ae760bd53b6e21f1f3758e2
torbjomg <torbjorn.gulbrandsen@fremtind.no>
2023-04-18 12:11:31 +0000

    clean up

--- services/web/app.py
+++ services/web/app.py
@@ -1,9 +1,9 @@
+import json
 import os
 import uuid
 from flask import Flask, redirect, url_for, session, request, render_template
 import msal
 from dotenv import load_dotenv
-from base64 import b64encode
 
 load_dotenv()
 app = Flask(__name__)
@@ -13,21 +13,19 @@ CLIENT_ID = os.environ.get("AAD_CLIENT_ID")
 TENANT_ID = os.environ.get("AAD_TENANT_ID")
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 REDIRECT_PATH = "/"
-SCOPE = ["https://graph.microsoft.com/User.Read"]
+SCOPE = ["https://sparebank1-fremtind-sandbox.snowflakecomputing.com/session:role-any"]
 SESSION_KEY = "user"
+REDIRECT_URI = "http://localhost:1234/redirect"
 
 
 @app.route("/")
 def index():
-    if app.debug is True:
-        dummy_token = b64encode(os.urandom(1024)).decode("utf-8").replace("/", "x")
-        return render_template("token.html", token=dummy_token, dummy=True)
-
     if not session.get(SESSION_KEY):
         return redirect(url_for("login"))
 
     return render_template(
-        "token.html", token=session[SESSION_KEY]["access_token"], dummy=False
+        "token.html",
+        token=json.dumps(session[SESSION_KEY], indent=4, separators=(",\n", ":\n")),
     )
 
 
@@ -38,12 +36,12 @@ def login():
     return redirect(auth_url)
 
 
-@app.route(REDIRECT_PATH)
+@app.route("/redirect")
 def authorized():
     if request.args.get("state", "") != session.get("state"):
         return "State does not match", 400
     if "error" in request.args:
-        return request.args.get("error") + ": " + request.args.get("error_description")
+        return f"{request.args.get('error')}: {request.args.get('error_description')}"
 
     result = _build_msal_app().acquire_token_by_authorization_code(
         request.args["code"],
@@ -52,7 +50,7 @@ def authorized():
     )
 
     if "error" in result:
-        return result["error"] + ": " + result["error_description"]
+        return f"{result['error']}: {result['error_description']}"
 
     session[SESSION_KEY] = result
     return redirect(url_for("index"))
@@ -60,7 +58,7 @@ def authorized():
 
 def _build_auth_url(scope=None, state=None):
     return _build_msal_app().get_authorization_request_url(
-        SCOPE, state=state, redirect_uri=url_for("authorized", _external=True)
+        SCOPE, state=state, redirect_uri=REDIRECT_URI
     )
 
 
@@ -71,4 +69,4 @@ def _build_msal_app():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=1234)
+    app.run(debug=False, port=1234)
